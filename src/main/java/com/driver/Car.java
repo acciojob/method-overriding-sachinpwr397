70b253a6f9d35b4c93074889befc1bb95375fed7
sachinpwr397 <79899346+sachinpwr397@users.noreply.github.com>
2024-04-06 12:54:29 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -2,6 +2,6 @@ package com.driver;
 public class Car extends Vehicle {
     @Override
     public void start() {
-        System.out.println("Car is starting...");
+        System.out.println("Car is starting....");
     }
 }
