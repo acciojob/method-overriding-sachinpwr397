dccbb972464cf9d0e9c0e191155dbb8f927e9785
sachinpwr397 <79899346+sachinpwr397@users.noreply.github.com>
2024-04-06 12:59:31 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,13 +2,11 @@ package com.driver;
 
 public class Main {
    public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle();
-        vehicle.start(); // Output: Vehicle is starting...
+        // Task 3: Create an object of class B and call method meth of class A with it
+        B objectB = new B();
+        System.out.println(objectB.meth()); // Output: Invoking method from class A
         
-        Car car = new Car();
-        car.start(); // Output: Car is starting...
-
-        // Polymorphism example
-        Vehicle anotherCar = new Car();
-        anotherCar.start(); // Output: Car is starting...
+        // Task 5: Call the overridden method from object of class B
+        System.out.println(objectB.meth()); // Output: Method is overridden in Extended class B
+    }
 }
