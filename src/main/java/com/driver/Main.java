ffadbbf1586b4fcf65162e0f7b3a5578a02c99d5
sachinpwr397 <79899346+sachinpwr397@users.noreply.github.com>
2024-04-06 12:47:49 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+   public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.start(); // Output: Vehicle is starting...
+        
+        Car car = new Car();
+        car.start(); // Output: Car is starting...
+
+        // Polymorphism example
+        Vehicle anotherCar = new Car();
+        anotherCar.start(); // Output: Car is starting...
 }
