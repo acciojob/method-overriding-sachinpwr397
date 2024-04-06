0929f396c29cba30e8073e120375ae03d2015cf9
sachinpwr397 <79899346+sachinpwr397@users.noreply.github.com>
2024-04-06 12:58:07 +0530

    Update and rename Car.java to B.java

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,8 @@
+package com.driver;
+class B extends A {
+    // Task 4: Override method meth of class A in class B
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
--- src/main/java/com/driver/Car.java
+++
@@ -1,7 +0,0 @@
-package com.driver;
-public class Car extends Vehicle {
-    @Override
-    public void start() {
-        System.out.println("Car is starting....");
-    }
-}
